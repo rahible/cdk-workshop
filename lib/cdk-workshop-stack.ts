1ef5367c77964bdb2eaebf004f00a193e80f288a
R. Aaron Hible <rahible@gmail.com>
2021-02-13 07:53:38 -0600

    left off with trying to get the counter to work, current error is that the wrapper lambda is getting permission denied calling the wrapped lambda

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import {HitCounter} from "./hitcounter";
 
 export class CdkWorkshopStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -12,9 +13,13 @@ export class CdkWorkshopStack extends cdk.Stack {
             code: lambda.Code.fromAsset('lambda'),
             handler: 'hello.handler'
         });
- 
+
+        const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+            downstream: hello
+        });
+
         new apigw.LambdaRestApi(this, 'Endpoint', {
-            handler: hello
+            handler: helloWithCounter.handler
         });
     }
 }
